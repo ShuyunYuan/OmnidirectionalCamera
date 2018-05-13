fe171265cd95dc348aa5c048e25fad8153ee6612
Yuan Shuyun <yuanshuyun1012@gmail.com>
2018-05-13 01:33:59 -0700

    [Feature] Add threshold parameter and UI.

--- viewer.cpp
+++ viewer.cpp
@@ -19,21 +19,17 @@ Viewer::~Viewer() {
 void Viewer::on_openButton_clicked() {
     fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QString(),
             tr("Image Files (*.bmp *.jpeg *.jpg *.png)"));
+    undistort();
+}
 
-    if (fileName.isEmpty()) {
-        return;
-    }
-    QPixmap inputPixmap;
-    inputPixmap.load(fileName);
-    ui->inputImage->setPixmap(inputPixmap.scaled(600, 600, Qt::KeepAspectRatio, Qt::SmoothTransformation));
-    cv::Mat matrix = undistortPanorama(fileName.toLocal8Bit().constData());
-    cv::cvtColor(matrix, matrix, matrix.type() == CV_8UC1 ? CV_GRAY2RGB : CV_BGR2RGB);
-    outputPixmap = QPixmap::fromImage(QImage(matrix.data, matrix.cols, matrix.rows, matrix.cols * 3,
-            QImage::Format_RGB888));
-    ui->outputImage->setPixmap(outputPixmap.scaled(600, 600, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+void Viewer::on_updateThresholdButton_clicked() {
+    undistort();
 }
 
 void Viewer::on_saveButton_clicked() {
+    if (outputPixmap.isNull()) {
+        return;
+    }
     QFileInfo fileInfo(fileName);
     QString saveFileName = QDir(fileInfo.path()).filePath("undistorted_" +
             fileInfo.fileName().remove(QRegExp("^test_")));
@@ -42,4 +38,19 @@ void Viewer::on_saveButton_clicked() {
     if (!saveFileName.isEmpty()) {
         outputPixmap.save(saveFileName);
     }
+}
+
+void Viewer::undistort() {
+    if (fileName.isEmpty()) {
+        return;
+    }
+    QPixmap inputPixmap;
+    inputPixmap.load(fileName);
+    ui->inputImage->setPixmap(inputPixmap.scaled(600, 600, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    int threshold = ui->thresholdSpinBox->value();
+    cv::Mat matrix = undistortPanorama(fileName.toLocal8Bit().constData(), threshold);
+    cv::cvtColor(matrix, matrix, matrix.channels() == 1 ? CV_GRAY2RGB : CV_BGR2RGB);
+    outputPixmap = QPixmap::fromImage(QImage(matrix.data, matrix.cols, matrix.rows, matrix.cols * 3,
+                                             QImage::Format_RGB888));
+    ui->outputImage->setPixmap(outputPixmap.scaled(600, 600, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 }
