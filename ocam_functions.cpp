499478eda460195d403b6db45b0944c8fda4ac4d
Yuan Shuyun <yuanshuyun1012@gmail.com>
2018-05-12 23:25:31 -0700

    [Refactor] Refactoring in progress.

--- ocam_functions.cpp
+++ ocam_functions.cpp
@@ -1,27 +1,20 @@
-
-#define _USE_MATH_DEFINES
-#include <math.h>
+#include <cmath>
 
 #include "ocam_functions.h"
 
-
 //生成全景环带的展开图
 void create_panoramic_undistortion_LUT(CvMat *mapx, CvMat *mapy, float Rmin, float Rmax, float xc, float yc) {
-    int i, j;
-    float theta;
     int width = mapx->width;
     int height = mapx->height;
-    float *data_mapx = mapx->data.fl;
-    float *data_mapy = mapy->data.fl;
-    float rho;
-
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < width; j++) {
-            // Note, if you would like to flip the image, just inverte the sign of theta
-            theta = -((float) j) / width * 2 * M_PI;
-            rho = Rmax - (Rmax - Rmin) / height * i;
-            *(data_mapx + i * width + j) = xc + rho * sin(theta);
-            *(data_mapy + i * width + j) = yc + rho * cos(theta);
+    float *mapx_data = mapx->data.fl;
+    float *mapy_data = mapy->data.fl;
+    for (int row = 0; row < height; ++row) {
+        for (int column = 0; column < width; ++column) {
+            float theta = ((float) column) / width * 2 * M_PI;
+            float rho = Rmax - (Rmax - Rmin) / height * row;
+            int index = row * width + column;
+            mapx_data[index] = xc + rho * cos(theta);
+            mapy_data[index] = yc + rho * sin(theta);
         }
     }
 }
