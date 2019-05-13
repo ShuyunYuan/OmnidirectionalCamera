57c5eeb79e8383d3518e58921adb1863c4dcd42d
Yuan Shuyun <yuanshuyun1012@gmail.com>
2019-05-12 23:43:46 -0700

    [Feature] Make the program headless.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
-#include <QApplication>
+#include <iostream>
 
-#include "viewer.h"
+#include <opencv2/opencv.hpp>
+
+#include "processor.h"
+
+using namespace std;
+using namespace cv;
 
 int main(int argc, char *argv[]) {
-    QApplication application(argc, argv);
-    Viewer viewer;
-    viewer.show();
-    return QApplication::exec();
+
+    if (!(argc == 2 || argc == 3)) {
+        cerr << "Usage: " << argv[0] << " <FILE_NAME> [THRESHOLD]" << endl;
+        exit(1);
+    }
+    char *fileName = argv[1];
+    double threshold = argc == 3 ? stod(argv[2]) : 70;
+
+    Mat outputImage = undistortPanorama(fileName, threshold);
+    string outputFileName = fileName;
+    string::size_type lastSeparatorIndex = outputFileName.find_last_of("/\\");
+    string::size_type prefixIndex = lastSeparatorIndex != string::npos ? lastSeparatorIndex + 1 : 0;
+    outputFileName.insert(prefixIndex, "undistorted_");
+    imwrite(outputFileName, outputImage);
 }
